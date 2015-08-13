56ce36e429f9e97ca2e8339a783352de2f12abe6
rajgit2012 <rajnsu08@gmail.com>
2015-08-13 12:36:42 +0100

    test changes

--- app/src/main/java/com/upd8r/mytestapplication/MainActivity.java
+++ app/src/main/java/com/upd8r/mytestapplication/MainActivity.java
@@ -34,4 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void testGit(){
+        System.out.println("Lets test git!");
+    }
 }
